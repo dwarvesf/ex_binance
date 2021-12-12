16316c13cdb82e18c71485b3ad4accc763ccac02
An Tran <tienan92it@gmail.com>
2021-12-12 16:50:08 +0700

    feat: add batch orders for future apis

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binancex.MixProject do
   def project do
     [
       app: :dwarves_binancex,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
