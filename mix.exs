39e40da82ce236434b43183dc1075c03f534be3a
An Tran <tienan92it@gmail.com>
2022-03-30 16:19:50 +0700

    chore: Bump version 0.1.14

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binancex.MixProject do
   def project do
     [
       app: :dwarves_binancex,
-      version: "0.1.13",
+      version: "0.1.14",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
