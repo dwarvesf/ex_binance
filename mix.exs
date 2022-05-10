ec54f83fd080aefd9e5d7041d0a04936cdc0ddc1
An Tran <tienan92it@gmail.com>
2022-05-10 21:57:50 +0700

    Bump version 0.1.17

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binancex.MixProject do
   def project do
     [
       app: :dwarves_binancex,
-      version: "0.1.16",
+      version: "0.1.17",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
