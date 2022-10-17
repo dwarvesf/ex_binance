ce2743ee881609f0c58bd94e0db4d784ace47474
Minh Trần <thminhVN@users.noreply.github.com>
2022-10-17 10:40:10 +0700

    feat: spot account info (#19)

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 config :dwarves_binancex,
   perpetual_futures_url: "https://fapi.binance.com",
